eeeb716aaddd90d4520191c259ff2fe790601490
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-10-21 11:00:24 -0400

    wrap card in Link

--- src/components/Card/LargeCard/LargeCard.js
+++ src/components/Card/LargeCard/LargeCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles.css";
@@ -7,32 +8,34 @@ import SubNav from "../../NavBar/SubNav/SubNav";
 
 const TYPES = ["card--user", "card--quiz", "card--platform"];
 
-const LargeCard = ({ children, cardType, cardInfo }) => {
+const LargeCard = ({ children, cardType, cardInfo, cardLink }) => {
   return (
-    <div className="card d-flex flex-column">
-      <div className="topCard d-flex flex-row">
-        <div>
-          <img className="icon" src="/propic.png" alt="icon" />
-        </div>
-        <div className="info flex-grow-1">
-          <div className="title">{cardInfo.title}</div>
-          <div className="description">{cardInfo.description}</div>
-        </div>
-      </div>
-      <div className="bottomCard d-flex flex-row justify-content-between align-items-end">
-        <div className="description subtext">{cardInfo.subtext}</div>
-        <div className="feedback d-flex flex-row">
-          <div className="feedbackItem d-flex flex-row align-items-center">
-            <img className="feedbackImage" src="/like.png" alt="like" />
-            <div className="count">1000</div>
+    <Link className="link" to={cardLink}>
+      <div className="card d-flex flex-column">
+        <div className="topCard d-flex flex-row">
+          <div>
+            <img className="icon" src="/propic.png" alt="icon" />
           </div>
-          <div className="feedbackItem d-flex flex-row align-items-center">
-            <img className="feedbackImage" src="/dislike.png" alt="dislike" />
-            <div className="count">1000</div>
+          <div className="info flex-grow-1">
+            <div className="title">{cardInfo.title}</div>
+            <div className="description">{cardInfo.description}</div>
           </div>
         </div>
+        <div className="bottomCard d-flex flex-row justify-content-between align-items-end">
+          <div className="description subtext">{cardInfo.subtext}</div>
+          <div className="feedback d-flex flex-row">
+            <div className="feedbackItem d-flex flex-row align-items-center">
+              <img className="feedbackImage" src="/like.png" alt="like" />
+              <div className="count">1000</div>
+            </div>
+            <div className="feedbackItem d-flex flex-row align-items-center">
+              <img className="feedbackImage" src="/dislike.png" alt="dislike" />
+              <div className="count">1000</div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
