c1e15f4124dd76cde12c55c6ad825368a4f6e6f1
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-10-21 15:47:42 -0400

    move feed to pages section

---
+++ src/pages/Feed/Feed.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+import Button from "../../components/Button/Button";
+import SubNav from "../../components/NavBar/SubNav/SubNav";
+
+const Feed = () => {
+  const subNavButtons = [
+    <Link to="/createPlatform">
+      <Button>Create A Platform</Button>
+    </Link>,
+    <Link to="/Notifications">
+      <Button>Notifications</Button>
+    </Link>,
+  ];
+  return (
+    <div>
+      <SubNav heading="Welcome Back John1!" buttons={subNavButtons} />
+    </div>
+  );
+};
+
+export default Feed;
