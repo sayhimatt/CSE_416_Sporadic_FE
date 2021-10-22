807d6e1bc64ba82cc1a580274fb076871101e936
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-10-21 22:14:45 -0400

    add login to routes

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import MainNav from "./components/NavBar/MainNav/MainNav";
 import Footer from "./components/Footer/Footer";
-import Feed from "./Feed";
+import Feed from "./pages/Feed/Feed";
+import Login from "./pages/Login/MainLogin/MainLogin";
 
 const MainRouter = () => {
   return (
     <Router>
+      <Route path="/login" component={Login} />
       <MainNav />
       <Route path="/Homepage" component={Feed}></Route>
       <Route path="/About"></Route>
