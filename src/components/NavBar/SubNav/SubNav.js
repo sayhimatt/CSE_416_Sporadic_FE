fb9bd32066c270e4a292641147780d1e7a445452
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-10-23 11:41:52 -0400

    render children for subnav for component composition

--- src/components/NavBar/SubNav/SubNav.js
+++ src/components/NavBar/SubNav/SubNav.js
@@ -4,6 +4,7 @@ import "./styles.css";
 const SubNav = ({ children, heading, buttons }) => {
   return (
     <div className="subNav">
+      {children}
       <div className="heading">{heading}</div>
       <div className="buttons">{buttons}</div>
     </div>
