0cee7399ad56e703352157905e7307be42b25244
Matthew-Montalbano <matthew.montalbano@stonybrook.edu>
2021-10-24 15:29:10 -0400

    Feat/authstore (#16)
    * create global context for authentication
    
    * replace auth state in App with auth context
    
    * persistent login until explicit logout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Auth } from "aws-amplify";
 
+import AuthContextProvider from "./contexts/AuthContext";
 import GuardedRoute from "./components/GuardedRoute/GuardedRoute";
 import Feed from "./pages/Feed/Feed";
 import Login from "./pages/Login/MainLogin/MainLogin";
@@ -11,68 +11,23 @@ import ConfirmEmail from "./pages/Login/ConfirmEmail";
 import "./App.scss";
 
 const MainRouter = () => {
-  const [auth, setAuth] = useState(false);
-
-  useEffect(() => {
-    onLoad();
-  }, []);
-
-  const onLoad = async () => {
-    try {
-      await Auth.currentSession();
-      console.log("Logged in");
-      setAuth(true);
-    } catch (e) {
-      console.log("Failed login");
-    }
-  };
-
-  const logout = async () => {
-    await Auth.signOut();
-    setAuth(false);
-  };
-
-  const login = async (username, password) => {
-    try {
-      const success = await Auth.signIn(username, password);
-      console.log(success);
-      setAuth(true);
-    } catch (error) {
-      console.log("error signing in", error);
-      window.alert("Invalid Login");
-    }
-  };
-
   return (
-    <Router>
-      <Switch>
-        <Route
-          path="/login"
-          component={() => <Login auth={auth} loginHandler={login} />}
-        />
-        <Route exact path="/createAccount" component={CreateAccount} />
-        <Route
-          exact
-          path="/createAccount/confirmation"
-          component={ConfirmEmail}
-        />
-        <GuardedRoute
-          path="/"
-          auth={auth}
-          component={() => <Feed logoutHandler={logout} />}
-        ></GuardedRoute>
-        <GuardedRoute
-          path="/About"
-          auth={auth}
-          component={() => <Feed logoutHandler={logout} />}
-        ></GuardedRoute>
-        <GuardedRoute
-          path="/Contact"
-          auth={auth}
-          component={() => <Feed logoutHandler={logout} />}
-        ></GuardedRoute>
-      </Switch>
-    </Router>
+    <AuthContextProvider>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route exact path="/createAccount" component={CreateAccount} />
+          <Route
+            exact
+            path="/createAccount/confirmation"
+            component={ConfirmEmail}
+          />
+          <GuardedRoute path="/" component={Feed}></GuardedRoute>
+          <GuardedRoute path="/About" component={Feed}></GuardedRoute>
+          <GuardedRoute path="/Contact" component={Feed}></GuardedRoute>
+        </Switch>
+      </Router>
+    </AuthContextProvider>
   );
 };
 
--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,11 +1,21 @@
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Auth } from "aws-amplify";
 
+import { AuthContext } from "../../contexts/AuthContext";
 import Button from "../../components/Button/Button";
 import SubNav from "../../components/NavBar/SubNav/SubNav";
 import MainNav from "../../components/NavBar/MainNav/MainNav";
 import Footer from "../../components/Footer/Footer";
 
-const Feed = ({ children, logoutHandler }) => {
+const Feed = ({ children }) => {
+  const { dispatch } = useContext(AuthContext);
+
+  const logout = async () => {
+    await Auth.signOut();
+    dispatch({ type: "LOGOUT" });
+  };
+
   const subNavButtons = [
     <Link to="/createPlatform">
       <Button>Create A Platform</Button>
@@ -13,7 +23,7 @@ const Feed = ({ children, logoutHandler }) => {
     <Link to="/Notifications">
       <Button>Notifications</Button>
     </Link>,
-    <Button buttonStyle="btn--special" onClick={logoutHandler}>
+    <Button buttonStyle="btn--special" onClick={logout}>
       Sign Out
     </Button>,
   ];
