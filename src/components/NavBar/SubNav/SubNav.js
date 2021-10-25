fab28cde128b17767fd1c2be59f6ed2973492633
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-10-25 15:21:19 -0400

    refactor subnav layout

--- src/components/NavBar/SubNav/SubNav.js
+++ src/components/NavBar/SubNav/SubNav.js
@@ -5,8 +5,10 @@ const SubNav = ({ children, heading, buttons }) => {
   return (
     <div className="subNav">
       {children}
-      <div className="heading">{heading}</div>
-      <div className="buttons">{buttons}</div>
+      <div className="d-flex justify-content-between flex-grow-1">
+        <div className="heading">{heading}</div>
+        <div className="buttons">{buttons}</div>
+      </div>
     </div>
   );
 };
