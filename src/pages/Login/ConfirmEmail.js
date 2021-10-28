968c814b0577a246e28920d12d38dbf268acd4a7
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-10-28 10:53:07 -0400

    remove unnecessary axios import

--- src/pages/Login/ConfirmEmail.js
+++ src/pages/Login/ConfirmEmail.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 import { useHistory } from "react-router";
 
 import { postConfirmCode } from "../../API/API";
