605f28b06326781600e84ac5a0ddc688f14cd834
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-01 17:34:43 -0400

    reuse loading spinner code in overlay

--- src/components/LoadingIndicators/LoadingOverlay.js
+++ src/components/LoadingIndicators/LoadingOverlay.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactComponent as Spore } from "../../spore.svg";
+import LoadingSpinner from "./LoadingSpinner";
 import "./style.css";
 
 /**
@@ -10,13 +10,7 @@ const LoadingOverlay = ({ isVisible, width, spinSpeed }) => {
     <>
       {isVisible && (
         <div className="overlay">
-          <Spore
-            style={{
-              animation: `spin ${spinSpeed || 3}s linear infinite`,
-              height: "auto",
-              width: width || "15%",
-            }}
-          />
+          <LoadingSpinner isVisible={isVisible} width={width} spinSpeed={spinSpeed} />
         </div>
       )}
     </>
