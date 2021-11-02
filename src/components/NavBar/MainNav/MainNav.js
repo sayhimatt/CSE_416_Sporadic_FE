d0608bb6baccca3f33784d0a8e683f9670c0c05e
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-11-02 11:38:15 -0400

    get rid of invisible div behind navbar and make navbar position sticky

--- src/components/NavBar/MainNav/MainNav.js
+++ src/components/NavBar/MainNav/MainNav.js
@@ -8,29 +8,26 @@ import Feed from "../../../pages/Feed/Feed";
 const NavBar = () => {
   const { auth, dispatch } = useContext(AuthContext);
   return (
-    <div>
-      <div className="navBar">
-        <div className="logo">
-          <Link to="/">
-            <img src="/logo.svg" alt="placeholder" />
-          </Link>
-        </div>
-        <div className="searchBar">
-          <input className="search" placeholder="Search"></input>
-        </div>
-        <div className="rightNav">
-          <div className="rightNavItems">
-            <div className="account">
-              <img className="profilePicture" src="/propic.png" alt="placeholder" />
-              <div className="navText">{auth.username}</div>
-            </div>
-            <div className="subscriptionDropdown">
-              <div className="navText">Subscription</div>
-            </div>
+    <div className="navBar">
+      <div className="logo">
+        <Link to="/">
+          <img src="/logo.svg" alt="placeholder" />
+        </Link>
+      </div>
+      <div className="searchBar">
+        <input className="search" placeholder="Search"></input>
+      </div>
+      <div className="rightNav">
+        <div className="rightNavItems">
+          <div className="account">
+            <img className="profilePicture" src="/propic.png" alt="placeholder" />
+            <div className="navText">{auth.username}</div>
+          </div>
+          <div className="subscriptionDropdown">
+            <div className="navText">Subscription</div>
           </div>
         </div>
       </div>
-      <div className="page-alignment-fixer" />
     </div>
   );
 };
