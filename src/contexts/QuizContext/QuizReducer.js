3df8ad975112917a8675227a980fb9c96d41cd7c
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-11-04 23:40:51 -0400

    extract actions into methods

--- src/contexts/QuizContext/QuizReducer.js
+++ src/contexts/QuizContext/QuizReducer.js
@@ -3,48 +3,61 @@ import React from "react";
 const QuizReducer = (state, action) => {
   switch (action.type) {
     case "SET_QUESTION_TITLE":
-      const { id, title } = action.payload;
-      const questions = state.questions;
-      questions[id].body = title;
-      return { ...state, questions };
+      return setQuestionTitle(state, action.payload);
 
     case "SET_CHOICE":
-      const { id, choiceNumber, text } = action.payload;
-      const questions = state.questions;
-      questions[id].answers[choiceNumber].text = text;
-      return { ...state, questions };
+      return setChoice(state, action.payload);
 
     case "SET_ANSWER":
-      const { id, choiceNumber } = action.payload;
-      const questions = state.questions;
-      questions[id].correctAnswer = choiceNumber;
-      return { ...state, questions };
+      return setAnswer(state, action.payload);
 
     case "SET_TITLE":
-      const { title } = action.payload;
-      return { ...state, title };
+      return { ...state, title: action.payload.title };
 
     case "SET_DESCRIPTION":
-      const { description } = action.payload;
-      return { ...state, description };
+      return { ...state, description: action.payload.description };
 
     case "SET_TIMER":
-      const { time } = action.payload;
-      return { ...state, timeLimit: time };
+      return { ...state, timeLimit: action.payload.timeLimit };
 
     case "ADD_QUESTION":
-      const questions = state.questions;
+      let questions = state.questions;
       questions.push();
 
     case "DELETE_QUESTION":
-      const { id } = action.payload;
-      const questions = state.questions;
-      questions.pop(id);
-      return { ...state, questions };
+      return deleteQuestion(state, action.payload);
 
     default:
       return state;
   }
+};
+
+const setQuestionTitle = (state, payload) => {
+  let { id, title } = payload;
+  let questions = state.questions;
+  questions[id].body = title;
+  return { ...state, questions };
+};
+
+const setChoice = (state, payload) => {
+  let { id, choiceNumber, text } = payload;
+  let questions = state.questions;
+  questions[id].answers[choiceNumber].text = text;
+  return { ...state, questions };
+};
+
+const setAnswer = (state, payload) => {
+  let { id, choiceNumber } = payload;
+  let questions = state.questions;
+  questions[id].correctAnswer = choiceNumber;
+  return { ...state, questions };
+};
+
+const deleteQuestion = (state, payload) => {
+  let { id } = payload;
+  let questions = state.questions;
+  questions.pop(id);
+  return { ...state, questions };
 };
 
 export default QuizReducer;
