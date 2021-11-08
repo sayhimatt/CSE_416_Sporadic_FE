3a555728f93830d69f0739a2b14a4088a8f98f21
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-11-08 10:38:41 -0500

    generalize question card for display or creation

--- src/components/Card/QuestionCard/QuestionCard.js
+++ src/components/Card/QuestionCard/QuestionCard.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import "./styles.css";
-import SubNav from "../../NavBar/SubNav/SubNav";
 
-const Question = ({ information }) => {
+const Question = ({ information, create }) => {
   const choiceCards = information.answers.map((answer, index) => {
     return (
       <div className="choice flex-row">
-        <div>{answer}</div>
+        <div className="d-flex flex-grow-1">
+          {create ? <textarea className="input" placeholder="Enter Answer"></textarea> : answer}
+        </div>
         <input
           className="form-check-input marker"
           type="radio"
@@ -16,16 +16,21 @@ const Question = ({ information }) => {
           name={"answer" + information.questionIndex}
         ></input>
       </div>
-    )
+    );
   });
-
-  
-
-
 
   return (
     <div className="cardContainer cardContainer--question flex-column align-items-center">
-      <div className="question">{information.question}</div>
+      <div className="question">
+        {create ? (
+          <textarea
+            className="input text-center bg-transparent"
+            placeholder="Enter Question"
+          ></textarea>
+        ) : (
+          information.question
+        )}
+      </div>
       <div className="d-flex flex-column flex-md-fill">{choiceCards}</div>
     </div>
   );
