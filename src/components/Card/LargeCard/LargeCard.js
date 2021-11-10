ee7286e316b40f0f4d9d7550011965619cba9403
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-11-09 21:20:58 -0500

    add mod options to quiz cards

--- src/components/Card/LargeCard/LargeCard.js
+++ src/components/Card/LargeCard/LargeCard.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "../styles.css";
 import "./styles.css";
-import SubNav from "../../NavBar/SubNav/SubNav";
-import img from "../../../movie.svg"
+import DropdownMenu from "../../Dropdown/DropdownMenu/DropdownMenu";
+import img from "../../../movie.svg";
 const TYPES = ["card--user", "card--quiz", "card--platform"];
 
-const LargeCard = ({ children, cardType, cardInfo, cardLink }) => {
-  
+const LargeCard = ({ children, modOptions, cardInfo, cardLink }) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
-    <Link className="cardContainer cardContainer--large link" to={cardLink}>
+    <div className="cardContainer cardContainer--large link">
       <div className="card d-flex flex-column">
         <div className="topCard d-flex flex-row">
           <div>
             <img className="icon" src={img} alt="what" />
           </div>
           <div className="info flex-grow-1">
-            <div className="title">{cardInfo.title}</div>
+            <Link className="link" to={cardLink}>
+              <div className="title">{cardInfo.title}</div>
+            </Link>
             <div className="description">{cardInfo.description}</div>
           </div>
+          {modOptions && (
+            <a className="mod-options" onClick={toggleDropdown}>
+              <img alt="dropdown" src="/three_dot_menu.svg" />
+              {showDropdown && (
+                <DropdownMenu>
+                  <div>Pin quiz</div>
+                  <div>Delete quiz</div>
+                </DropdownMenu>
+              )}
+            </a>
+          )}
         </div>
         <div className="bottomCard d-flex flex-row justify-content-between align-items-end">
           <div className="description subtext">{cardInfo.subtext}</div>
@@ -35,7 +53,7 @@ const LargeCard = ({ children, cardType, cardInfo, cardLink }) => {
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
