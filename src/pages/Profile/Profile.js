ae355debb7f18c4d0b2895ffc8c4c9c162fa46e7
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-11-23 10:45:58 -0500

    add/remove friends or edit profile through profile page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -2,9 +2,10 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link, useParams, useHistory } from "react-router-dom";
 
 import { UserContext } from "../../contexts/UserContext/UserContext";
-import { getUser, getUserIcon } from "../../API/API";
+import { getUser, getUserIcon, manageFriend } from "../../API/API";
 import NavBar from "../../components/NavBar/MainNav/MainNav";
 import Button from "../../components/Button/Button";
+import { Alert } from "react-bootstrap";
 
 import "./styles.scss";
 
@@ -13,6 +14,7 @@ const Profile = () => {
   const [userState, setuserState] = useState();
   const [friends, setFriends] = useState();
   const [avatar, setAvatar] = useState();
+  const [alert, setAlert] = useState({ show: false, message: "", style: "danger" });
   const params = useParams();
   const history = useHistory();
 
@@ -35,28 +37,76 @@ const Profile = () => {
   const loadButton = () => {
     if (params.username === user.username) {
       return (
-        <Button buttonStyle="btn--secondary">
-          <b>Edit Profile</b>
-        </Button>
+        <Link to="/myAccount">
+          <Button buttonStyle="btn--secondary">
+            <b>Edit Profile</b>
+          </Button>
+        </Link>
       );
     } else if (friends.includes(params.username)) {
       return (
-        <Button buttonStyle="btn--secondary">
+        <Button buttonStyle="btn--secondary" onClick={removeFriend}>
           <b>Remove Friend</b>
         </Button>
       );
     } else {
       return (
-        <Button buttonStyle="btn--secondary">
+        <Button buttonStyle="btn--secondary" onClick={addFriend}>
           <b>Add friend</b>
         </Button>
       );
     }
   };
 
+  const addFriend = () => {
+    manageFriend(params.username, "add")
+      .then((res) => {
+        setFriends((prevState) => [...prevState, params.username]);
+        setAlert({
+          show: true,
+          style: "sporadic-secondary",
+          message: `${params.username} has been added as a friend`,
+        });
+      })
+      .catch((e) =>
+        setAlert({
+          show: true,
+          style: "danger",
+          message: `${params.username} could not be added as a friend`,
+        }),
+      );
+  };
+
+  const removeFriend = () => {
+    manageFriend(params.username, "remove")
+      .then((res) => {
+        setFriends((prevState) => prevState.filter((user) => user !== params.username));
+        setAlert({
+          show: true,
+          style: "sporadic-secondary",
+          message: `${params.username} has been removed from your friends`,
+        });
+      })
+      .catch((e) =>
+        setAlert({
+          show: true,
+          style: "danger",
+          message: `${params.username} could not be removed from your friends`,
+        }),
+      );
+  };
+
   return (
     <div>
       <NavBar />
+      <Alert
+        show={alert.show}
+        variant={alert.style}
+        onClose={() => setAlert({ show: false, message: "", style: "" })}
+        dismissible
+      >
+        {alert.message}
+      </Alert>
       {userState && avatar && (
         <div className="page-content ms-5 me-5">
           <div className="d-flex flex-column justify-content-center align-items-center">
@@ -66,7 +116,7 @@ const Profile = () => {
               <div className="d-flex flex-column">{userState && friends && loadButton()}</div>
             </div>
             <div className="profile-section">
-              <h2>STATS</h2>
+              <h3>STATS</h3>
               <div className="d-flex w-50 justify-content-around">
                 <h3>
                   <b className="color-secondary">
@@ -80,13 +130,13 @@ const Profile = () => {
               </div>
             </div>
             <div className="profile-section">
-              <h2>ABOUT</h2>
+              <h3>ABOUT</h3>
               <div id="account-about-input" className="input-box w-75">
                 <div className="input">{userState.aboutSection}</div>
               </div>
             </div>
             <div className="profile-section">
-              <h2>AWARDS</h2>
+              <h3>AWARDS</h3>
               <div id="account-awards-shelf">
                 {userState.awards &&
                   userState.awards
