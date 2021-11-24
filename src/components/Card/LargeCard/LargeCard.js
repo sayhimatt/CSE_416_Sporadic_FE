0652579559d0419c9cbdd1c8ac6c58001159e4ad
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-11-24 10:32:45 -0500

    fix feedback alignment and update some styles

--- src/components/Card/LargeCard/LargeCard.js
+++ src/components/Card/LargeCard/LargeCard.js
@@ -1,23 +1,35 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import "../styles.css";
-import "./styles.css";
-import DropdownMenu from "../../Dropdown/DropdownMenu/DropdownMenu";
-import DropdownItem from "../../Dropdown/DropdownItem/DropdownItem";
 import CustomToggle from "../../CustomToggle/CustomToggle";
 import { Dropdown } from "react-bootstrap";
 
-const LargeCard = ({ children, iconSrc, modOptions, cardInfo, cardLink, dropdownHandlers }) => {
-  const [showDropdown, setShowDropdown] = useState(false);
+import "../styles.css";
+import "./styles.css";
 
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
+const LargeCard = ({ children, iconSrc, modOptions, cardInfo, cardLink, dropdownHandlers }) => {
+  const convertVotes = (votes) => {
+    const votesNum = parseInt(votes);
+    if (votes < 1000) {
+      return votes;
+    } else if (votes >= 1000 && votes < 10000) {
+      const votesString = (Math.round((votesNum / 1000) * 10) / 10).toFixed(2);
+      return `${votesString}k`;
+    } else if (votes >= 10000 && votes < 100000) {
+      const votesString = (Math.round((votesNum / 1000) * 100) / 100).toFixed(1);
+      return `${votesString}k`;
+    } else if (votes >= 100000 && votes < 1000000) {
+      const votesString = Math.round(votesNum / 1000).toString();
+      return `${votesString}k`;
+    } else {
+      const votesString = Math.round(votesNum / 1000000).toString();
+      return `${votesString}m`;
+    }
   };
 
   return (
     <div className="cardContainer cardContainer--large link">
-      <div className="card d-flex flex-column">
+      <div className="card d-flex flex-column justify-content-between">
         <div className="topCard d-flex flex-row">
           <div>
             <img className="icon" src={iconSrc} alt="what" />
@@ -57,15 +69,15 @@ const LargeCard = ({ children, iconSrc, modOptions, cardInfo, cardLink, dropdown
         </div>
         <div className="bottomCard d-flex flex-row justify-content-between align-items-end">
           <div className="description subtext">{cardInfo.subtext}</div>
-          {cardInfo.upvotes && cardInfo.downvotes && (
+          {cardInfo.upvotes !== undefined && cardInfo.downvotes !== undefined && (
             <div className="feedback d-flex flex-row">
               <div className="feedbackItem d-flex flex-row align-items-center">
                 <img className="feedbackImage" src="/like.png" alt="like" />
-                <div className="count">{cardInfo.upvotes}</div>
+                <div className="count">{convertVotes(cardInfo.upvotes)}</div>
               </div>
               <div className="feedbackItem d-flex flex-row align-items-center">
                 <img className="feedbackImage" src="/dislike.png" alt="dislike" />
-                <div className="count">{cardInfo.downvotes}</div>
+                <div className="count">{convertVotes(cardInfo.downvotes)}</div>
               </div>
             </div>
           )}
