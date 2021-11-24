2951b8863d4960a0e821fbb762236f68bebabcfe
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-11-24 17:12:56 -0500

    run everything through prettier

--- src/components/ErrorMessage/ErrorMessage.js
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -1,21 +1,20 @@
 import React from "react";
 
-
 //import "../styles.css";
 import "./styles.css";
 
 const STYLES = ["errorBox", "errorText"];
 
-const ErrorMessage = ({visible, errorStyle, text}) => {
+const ErrorMessage = ({ visible, errorStyle, text }) => {
   const checkErrorStyle = STYLES.includes(errorStyle) ? errorStyle : STYLES[0];
 
   if (visible) {
     return (
-        <div className= {`${checkErrorStyle}`}>
-          <p> {text} </p>
-        </div>
-      );
-  } else return (null);  
+      <div className={`${checkErrorStyle}`}>
+        <p> {text} </p>
+      </div>
+    );
+  } else return null;
 };
 
 export default ErrorMessage;
