450371c2a13bbd6f9a7130f33bdd523072531a7d
Jackson Ludwig <42984254+jacksonludwig@users.noreply.github.com>
2021-11-26 12:24:49 -0500

    adjust modal position and change colors to use root theme (#105)

--- src/components/Modals/InfoModal/InfoModal.js
+++ src/components/Modals/InfoModal/InfoModal.js
@@ -7,7 +7,7 @@ import "./styles.css";
  */
 const InfoModal = ({ title, body, buttonText, onButtonClicked, isVisible }) => {
   return (
-    <Modal show={isVisible} backdrop="static" keyboard={false}>
+    <Modal show={isVisible} backdrop="static" keyboard={false} style={{ top: "20%" }}>
       <Modal.Header>
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
@@ -17,7 +17,7 @@ const InfoModal = ({ title, body, buttonText, onButtonClicked, isVisible }) => {
       </Modal.Body>
 
       <Modal.Footer>
-        <Button variant="primary" onClick={onButtonClicked}>
+        <Button variant="confirm" onClick={onButtonClicked}>
           {buttonText}
         </Button>
       </Modal.Footer>
