4afdc51c5775f8f80fcf625cf8d82c34b197f2ee
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-12-08 16:25:13 -0500

    render awards in case and on display shelf; update styles

--- src/pages/AwardCase/AwardCase.js
+++ src/pages/AwardCase/AwardCase.js
@@ -14,9 +14,46 @@ const AwardCase = () => {
 
   useEffect(() => {
     getUser(user.username)
-      .then((res) => setAwards({ onDisplay: res.showcasedAwards, notOnDisplay: res.awards }))
-      .catch((e) => console.log(error));
+      .then((res) => {
+        const showCased = res.showCasedAwards ? res.showCasedAwards : [];
+        const awards = res.awards ? res.awards : [];
+        setAwards({
+          onDisplay: showCased,
+          notOnDisplay: awards,
+        });
+      })
+      .catch((e) => console.log(e));
   }, []);
+
+  const renderCase = () => {
+    const awardRows = splitAwardsIntoRows();
+    return (
+      <div id="award-case">
+        {awardRows.map((row) => (
+          <div className="case-row">
+            <div className="cased-awards">
+              {row.map((award) => (
+                <div className="award-container">
+                  <img className="award-image cased-award" src={award.image} alt="award" />
+                </div>
+              ))}
+            </div>
+            <div className="shelf" />
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const splitAwardsIntoRows = () => {
+    const awardsPerRow = 8;
+    const rows = Math.ceil(awards.notOnDisplay.length / awardsPerRow);
+    const awardRows = [];
+    for (let i = 0; i < rows; i++) {
+      awardRows.push(awards.notOnDisplay.slice(i * awardsPerRow, i * awardsPerRow + awardsPerRow));
+    }
+    return awardRows;
+  };
 
   return !awards ? (
     <NavBar />
@@ -30,10 +67,19 @@ const AwardCase = () => {
       <div className="page-content">
         <div className="d-flex flex-column align-items-center">
           <div id="display-shelf">
-            <div className="displayed-awards"></div>
+            <div id="displayed-awards">
+              {awards.onDisplay.map((award) => (
+                <div className="award-container">
+                  <img className="award-image displayed-award" src={award.image} alt="award" />
+                  <div className="d-flex">
+                    <img className="delete-award" src="/question_delete.svg" alt="delete" />
+                  </div>
+                </div>
+              ))}
+            </div>
             <div className="shelf" />
           </div>
-          <div id="award-case"></div>
+          {renderCase()}
         </div>
       </div>
     </div>
