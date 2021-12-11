fdde8a2de384eac133ae80ba39be63dc2265f92c
Matthew Montalbano <matthew.montalbano@stonybrook.edu>
2021-12-11 15:37:17 -0500

    add infinite scroll

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,9 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import LoadingSpinner from "../LoadingIndicators/LoadingSpinner";
 
 import "./styles.scss";
 
-const Leaderboard = ({ scores = [], className }) => {
+const Leaderboard = ({ scores = [], hasMore, nextResultsHandler, className }) => {
+  const loadResults = () => {
+    return scores.map((score, index) => (
+      <div key={index} className="d-flex">
+        <div className="board-position leaderboard-cell">{score.position}</div>
+        <div className="board-name leaderboard-cell">
+          <Link className="link" to={`/user/${score.username}`}>
+            {score.username}
+          </Link>
+        </div>
+        <div className="board-score leaderboard-cell">{score.score}</div>
+      </div>
+    ));
+  };
+
   return (
     <div id="leaderboard" className={className}>
       <div id="leaderboard-heading">Leaderboard</div>
@@ -19,17 +35,19 @@ const Leaderboard = ({ scores = [], className }) => {
             <b>Score</b>
           </div>
         </div>
-        {scores.map((score, index) => (
-          <div key={index} className="d-flex">
-            <div className="board-position leaderboard-cell">{score.position}</div>
-            <div className="board-name leaderboard-cell">
-              <Link className="link" to={`/user/${score.username}`}>
-                {score.username}
-              </Link>
+        <InfiniteScroll
+          next={nextResultsHandler}
+          dataLength={scores.length}
+          hasMore={hasMore}
+          loader={
+            <div className="d-flex justify-content-center mt-4">
+              <LoadingSpinner isVisible={true} />
             </div>
-            <div className="board-score leaderboard-cell">{score.score}</div>
-          </div>
-        ))}
+          }
+          scrollThreshold={0.7}
+        >
+          {loadResults()}
+        </InfiniteScroll>
       </div>
     </div>
   );
